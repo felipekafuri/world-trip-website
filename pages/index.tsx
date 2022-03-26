ade6250dd53aa1f2b550fa05b251fcf6bb6880de
Felipe Ramos Kafuri <felipekafuri@MacBook-Pro.local>
2022-03-26 15:25:29 -0300

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,12 @@ import { Box, Container, Grid, GridItem } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import { Banner } from './components/Banner';
 import { LogoHeader } from './components/LogoHeader';
-
+import Image from 'next/image';
+import nightlife from '../public/assets/nightlife.png'
+import beach from '../public/assets/beach.png'
+import modern from '../public/assets/modern.png'
+import classic from '../public/assets/classic.png'
+import more from '../public/assets/more.png'
 
 const Home: NextPage = () => {
   return (
@@ -12,15 +17,41 @@ const Home: NextPage = () => {
       <Container maxW={1160} w="100%" h="100%">
         <Box w="100%" mt={40}>
           <Grid templateColumns='repeat(5, 1fr)' gap={6}>
-            <GridItem w='100%' h='100' bg='blue.500' />
-            <GridItem w='100%' h='100' bg='blue.500' />
-            <GridItem w='100%' h='100' bg='blue.500' />
-            <GridItem w='100%' h='100' bg='blue.500' />
-            <GridItem w='100%' h='100' bg='blue.500' />
-          </Grid>
-        </Box>
-      </Container>
+            <GridItem w='100%' h='100'>
+              <Image
+                src={nightlife}
+                alt="Nightlife"
+              />
+            </GridItem>
+
+            <GridItem w='100%' h='100'>
+              <Image
+                src={beach}
+                alt="Praia"
+              />
+            </GridItem>
+            <GridItem w='100%' h='100'>
+              <Image
+                src={modern}
+                alt="Moderno"
+              />
+            </GridItem>
+            <GridItem w='100%' h='100'>
+              <Image
+                src={classic}
+                alt="Clássico"
+              />
+            </GridItem>
+            <GridItem w='100%' h='100'>
+            <Image
+              src={more}
+              alt="Mais"
+            />
+          </GridItem>
+        </Grid>
     </Box>
+      </Container >
+    </Box >
   )
 }
 
